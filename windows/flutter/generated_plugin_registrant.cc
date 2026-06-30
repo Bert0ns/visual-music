85eddb602731a83f473d872b6312c5ef8621d8d1
bert0ns <davidebe03@gmail.com>
2026-06-30 12:06:57 +0200

    fix: trying to build on windows

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <projectm_texture/projectm_texture_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ProjectmTexturePluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ProjectmTexturePluginCApi"));
 }
